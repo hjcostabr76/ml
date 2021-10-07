72a857a3bd1d889a75e5278464e9f20c170a13f2
Hebert Costa <hjcosta.dev@gmail.com>
2021-10-07 18:19:30 -0300

    feat(nlp): rewrite tensor flow w2v example

--- nlp/w2v-ex-tensorflow/Word2Vec.py
+++ nlp/w2v-ex-tensorflow/Word2Vec.py
@@ -1,39 +1,35 @@
 import tensorflow as tf
-from tensorflow.keras import layers
 
-''''
-# Subclassed Word2Vec Model
-
-- Using the Keras Subclassing API;
-- The subclassed model enables to define the `call()` function that accepts (target, context) pairs;
-- (target, context) pairs can be passed into their corresponding embedding layer;
-
-Layers: target_embedding, context_embedding, dots, flatten.
-
-TODO: 2021-10-06 - What is this 'flatten' thing about?
-- tf.keras.layers.Flatten layer to flatten the results of dots layer into logits;
-...Reshape the `context_embedding` to perform a dot product with `target_embedding` and return the flattened result.
-
-'''
 class Word2Vec(tf.keras.Model):
-	
-	''' Layer that looks up the embedding of a word when it appears as a target_ word; '''
-	target_embedding: tf.keras.layers.Embedding = None
-	
-	''' Layer that looks up the embedding of a word when it appears as a context_ word; '''
-	context_embedding: tf.keras.layers.Embedding = None
+	''''
+		# Subclassed Word2Vec Model
+		@link: https://www.tensorflow.org/tutorials/text/word2vec#subclassed_word2vec_model
 
-	def __init__(self, vocab_size: int, embedding_dim: int, num_ns: int):
+		- Using the Keras Subclassing API;
+		- The subclassed model enables to define the `call()` function that accepts (target, context) pairs;
+		- (target, context) pairs can be passed into their corresponding embedding layer;
+
+		Layers: target_embedding, context_embedding, dots, flatten.
+
+		TODO: 2021-10-06 - What is this 'flatten' thing about?
+		- tf.keras.layers.Flatten layer to flatten the results of dots layer into logits;
+		...Reshape the `context_embedding` to perform a dot product with `target_embedding` and return the flattened result.
+
+	'''
+	
+	def __init__(self, vocab_size: int, embedding_dim: int, num_ns: int, layer_name_target: str):
 		super(Word2Vec, self).__init__()
 		
-		self.target_embedding = layers.Embedding(
+		# Layer that looks up the embedding of a word when it appears as a target_ word
+		self.target_embedding = tf.keras.layers.Embedding(
 			vocab_size,		# Number of parameters: (vocab_size * embedding_dim)
 			embedding_dim,
 			input_length=1,
-			name="w2v_embedding"
+			name=layer_name_target,
 		)
-		
-		self.context_embedding = layers.Embedding(
+
+		# Layer that looks up the embedding of a word when it appears as a context_ word	
+		self.context_embedding = tf.keras.layers.Embedding(
 			vocab_size,		# Number of parameters: (vocab_size * embedding_dim)
 			embedding_dim,
 			input_length= num_ns + 1
@@ -49,6 +45,6 @@ class Word2Vec(tf.keras.Model):
 		if len(target.shape) == 2:
 			target = tf.squeeze(target, axis=1)	# target: (batch,)
 		
-		word_emb = self.target_embedding(target)		# (batch, embed)
-		context_emb = self.context_embedding(context)	# (batch, context, embed)
+		word_emb = self.target_embedding(target)				# (batch, embed)
+		context_emb = self.context_embedding(context)			# (batch, context, embed)
 		return tf.einsum('be,bce->bc', word_emb, context_emb)	# dots: (batch, context)
