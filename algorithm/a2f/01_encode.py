67cf564426a47328f62e98d1ef62e887dab26f9c
Hebert Costa <hjcosta.dev@gmail.com>
2022-10-18 13:32:48 -0300

    feat(a2f): lpc encoding

--- algorithm/a2f/01_encode.py
+++ algorithm/a2f/01_encode.py
@@ -2,18 +2,20 @@ import os
 from . import LPC
 from . import paths
 
+import config
+import paths
 
-lpc = LPC(_num_worker=_num_worker)
+
+lpc = LPC(_num_worker=config.n_workers)
 wav_files = os.listdir(wav_path)
 
-for wav_file in wav_files:
-    if wav_file in skipped_files: continue
+for file in wav_files:
+    if file in skipped_files: continue
 
     try:
-        feature = lpc.wav2lpc(wav_file=os.path.join(wav_path, wav_file))
-        feature_file = os.path.join(feature_path, paths.feature_file(base_name))
-        np.save(feature_file, feature)
+        feature = lpc.wav2lpc(wav_file=paths.wav_file(file))
+        np.save(feature, paths.feature_file(file))
     
     except AssertionError as error:
         print(error)
-        print(f'failed file: "{wav_file}"')
+        print(f'failed file: "{file}"')
