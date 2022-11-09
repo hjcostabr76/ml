a330e51486d35d5121e6da4874502fa8fbd78a73
Hebert Costa <hjcosta.dev@gmail.com>
2022-11-09 13:21:38 -0300

    feat(dsys): proc-sin lista 03

---
+++ case/_exercise/dynamic-systems/22-02-list-03/q18/q18.m
@@ -0,0 +1,43 @@
+close all; clear; clc;
+
+b = [1 1];
+a = [1 -.5 .125];
+
+fs = 1;
+H = tf(b, a, fs);
+
+%% Unit sample response
+figure;
+impz(b, a);
+grid on;
+
+%% Zero pole map
+figure;
+pzmap(H);
+
+%% Frequency response
+
+% Denormalized
+[h, w] = freqz(b, a, 'whole');
+h = fftshift(h);
+w = w - pi;
+
+figure;
+subplot(2, 1, 1);
+plot(w, abs(h));
+title('Magnitude Response');
+ylabel('$|H(e^{j \omega})|$', 'Interpreter', 'latex');
+xlabel('$\omega$ (rad)', 'Interpreter', 'latex');
+grid on;
+
+subplot(2, 1, 2);
+plot(w, phase(h));
+title('Phase Response');
+ylabel('$\theta(\omega))$', 'Interpreter', 'latex');
+xlabel('$\omega$ (rad)', 'Interpreter', 'latex');
+grid on;
+
+% Normalized
+figure;
+freqz(b, a);
+grid on;
