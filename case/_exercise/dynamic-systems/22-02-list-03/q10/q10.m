78478081626ef608d7c45386121141724b074c0c
Hebert Costa <hjcosta.dev@gmail.com>
2022-11-09 10:44:53 -0300

    feat(dsys): proc-sin lista 03

--- case/_exercise/dynamic-systems/22-02-list-03/q10/q10.m
+++ case/_exercise/dynamic-systems/22-02-list-03/q10/q10.m
@@ -2,10 +2,10 @@ close all; clear; clc;
 
 b = [1/3 0];
 a = [1 -5/6 1/6];
-[h, w] = freqz(b, a, 'whole', 2001);
+[h, w] = freqz(b, a, 'whole');
 
+%% Response in radians
 figure;
-
 subplot(2, 1, 1);
 plot(w, abs(h));
 title('Magnitude Response');
@@ -18,4 +18,24 @@ plot(w, phase(h));
 title('Phase Response');
 ylabel('$\theta(\omega))$', 'Interpreter', 'latex');
 xlabel('$\omega$ (rad)', 'Interpreter', 'latex');
+grid on;
+
+%% Response in Hertz
+figure;
+
+fs = 5;
+f = (fs*w) / (2*pi);
+
+subplot(2, 1, 1);
+plot(f, abs(h));
+title('Magnitude Response');
+ylabel('$|H(f)|$', 'Interpreter', 'latex');
+xlabel('f (kHz)');
+grid on;
+
+subplot(2, 1, 2);
+plot(f, phase(h));
+title('Phase Response');
+ylabel('$\theta(f))$', 'Interpreter', 'latex');
+xlabel('f (kHz)');
 grid on;
