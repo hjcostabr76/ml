9b54a5ccec88f23634e0f3af4f8bd4f6666fb3bc
Hebert Costa <hjcosta.dev@gmail.com>
2022-12-12 19:54:14 -0300

    feat(dsys): proc-sin lista 04

---
+++ case/_exercise/dynamic-systems/22-02-list-04/q02/q02.m
@@ -0,0 +1,91 @@
+close all; clear all; clc;
+
+%% Filter params
+
+Rp = 1; % Max ripple in passing band [dB]
+Rs = 15; % Min attenuation on stop band [dB]
+
+% Digital frequencies in Herts
+fs = 10e3; % Sampling frequency
+omega_p = 2*pi*1e3 / fs;
+omega_s = 2*pi*1.5e3 / fs;
+
+% Digital frequencies in radians
+ws = pi*fs / 180;
+omega_p = pi*omega_p / 180;
+omega_s = pi*omega_s / 180;
+
+T = 1 / fs;
+Wp = 2/T * tan(omega_p / 2) / ws;
+Ws = 2/T * tan(omega_s / 2) / ws;
+
+%% Butterworth: Analog
+[n, Wn] = buttord(Wp, Ws, Rp, Rs);
+[num, den] = butter(n, Wn);
+
+figure;
+pzmap(tf(num, den));
+title('Butterworth: Analog');
+
+% Frequency responses
+[h_butt, w] = freqz(num, den, 'whole');
+% h_butt = fftshift(h_butt);
+% w = w - pi;
+
+figure;
+subplot(2, 1, 1);
+
+hold on;
+plot(w, abs(h_butt));
+hold off;
+
+title('Magnitude Response');
+ylabel('$|H(e^{j \omega})|$', 'Interpreter', 'latex');
+xlabel('$\omega$ (rad)', 'Interpreter', 'latex');
+grid on;
+
+subplot(2, 1, 2);
+
+hold on;
+plot(w, phase(h_butt));
+hold off;
+
+title('Phase Response');
+ylabel('$\theta(\omega))$', 'Interpreter', 'latex');
+xlabel('$\omega$ (rad)', 'Interpreter', 'latex');
+grid on;
+
+%% Butterworth: Digital
+[numd, dend] = bilinear(num, den, Wn);
+
+figure;
+pzmap(tf(numd, dend));
+title('Butterworth: Digital');
+
+% Frequency responses
+[h_butt, w] = freqz(num, den, 'whole');
+% h_butt = fftshift(h_butt);
+% w = w - pi;
+
+figure;
+subplot(2, 1, 1);
+
+hold on;
+plot(w, abs(h_butt));
+hold off;
+
+title('Magnitude Response');
+ylabel('$|H(e^{j \omega})|$', 'Interpreter', 'latex');
+xlabel('$\omega$ (rad)', 'Interpreter', 'latex');
+grid on;
+
+subplot(2, 1, 2);
+
+hold on;
+plot(w, phase(h_butt));
+hold off;
+
+title('Phase Response');
+ylabel('$\theta(\omega))$', 'Interpreter', 'latex');
+xlabel('$\omega$ (rad)', 'Interpreter', 'latex');
+grid on;
