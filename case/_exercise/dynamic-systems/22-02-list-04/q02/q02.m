d20cd6669687e4d455b876caaa7449f797be879a
Hebert Costa <hjcosta.dev@gmail.com>
2022-12-12 20:07:51 -0300

    feat(dsys): proc-sin lista 04

--- case/_exercise/dynamic-systems/22-02-list-04/q02/q02.m
+++ case/_exercise/dynamic-systems/22-02-list-04/q02/q02.m
@@ -15,22 +15,23 @@ ws = pi*fs / 180;
 omega_p = pi*omega_p / 180;
 omega_s = pi*omega_s / 180;
 
-T = 1 / fs;
+T = 1 / ws;
 Wp = 2/T * tan(omega_p / 2) / ws;
 Ws = 2/T * tan(omega_s / 2) / ws;
 
 %% Butterworth: Analog
 [n, Wn] = buttord(Wp, Ws, Rp, Rs);
 [num, den] = butter(n, Wn);
+Hs = tf(num, den);
 
 figure;
-pzmap(tf(num, den));
+pzmap(Hs);
 title('Butterworth: Analog');
 
 % Frequency responses
 [h_butt, w] = freqz(num, den, 'whole');
-% h_butt = fftshift(h_butt);
-% w = w - pi;
+h_butt = fftshift(h_butt);
+w = w - pi;
 
 figure;
 subplot(2, 1, 1);
@@ -57,15 +58,16 @@ grid on;
 
 %% Butterworth: Digital
 [numd, dend] = bilinear(num, den, Wn);
+Hz = tf(numd, dend, T);
 
 figure;
-pzmap(tf(numd, dend));
+pzmap(Hz);
 title('Butterworth: Digital');
 
 % Frequency responses
 [h_butt, w] = freqz(num, den, 'whole');
-% h_butt = fftshift(h_butt);
-% w = w - pi;
+h_butt = fftshift(h_butt);
+w = w - pi;
 
 figure;
 subplot(2, 1, 1);
